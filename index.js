3970397e779ca26ed032961d664bfea0b050ac27
futurist <jamesyang999@gmail.com>
2016-07-23 18:54:16 +0800

    ADD: fs.watch instead of chokidar

--- index.js
+++ index.js
@@ -1,19 +1,57 @@
-var chokidar = require('chokidar')
 var path = require('path')
+var fs = require('fs')
 var spawn = require('child_process').spawn
 
 var folder = 'D:\\Test'
 
-// Initialize watcher.
-var watcher = chokidar.watch(folder, {
-  ignored: /^\~\$/,
-  persistent: true,
-  depth: 0
-})
+try {
+  var CONFIG = require('../config.json')
+  folder = path.join(CONFIG.filesFolder, 'ÎÄ¼þ×ª»»')
+} catch(e) {}
 
-watcher.on( 'add', function(file) {
-  if(path.extname(file).toLowerCase().match(/x$/i) && !/^\~\$/.test(path.basename(file)) ) {
-    console.log('File', file, 'has been added')
-    spawn('cscript', ['oconv.vbs', file, path.join(folder, 'ok')])
+var fileStore = {}
+var INTERVAL = 300
+
+function getFile (name) {
+  return path.join(folder, name)
+}
+
+function checkFile (filename) {
+  var file = getFile(filename)
+  var store = fileStore[filename]
+  fs.stat(file, function (err, stats) {
+    if (store.stats.size == stats.size) {
+      convertFile(file)
+    } else {
+      store.stats = stats
+      setTimeout(function () {
+        checkFile(filename)
+      }, INTERVAL)
+    }
+  })
+}
+
+fs.watch(folder, {persistent: true}, function (event, filename) {
+  // add or delete
+  if (event == 'rename') {
+    // it's second time rename (delete)
+    if (filename in fileStore) {
+      delete fileStore[filename]
+    } else {
+      fileStore[filename] = {stats: {}}
+      checkFile(filename)
+    }
   }
 })
+
+console.log('watching folder for office files', folder)
+
+function convertFile (file) {
+  if( /^\~\$/.test(path.basename(file)) ) return
+  if (path.extname(file).toLowerCase().match(/x$/i)) {
+    console.log('File', file, 'has been added')
+    spawn('cscript', ['oconv.vbs', file, path.join(folder, 'ok')])
+  } else {
+    fs.unlink(file)
+  }
+}
---
+++ lyoconv.cmd
@@ -0,0 +1,9 @@
+@rem HIDE CMD WINDOW if you don't want to see cmd
+title lyoconv
+@ping /n 2 127.1>nul
+@nircmd.exe win hide title "lyoconv"
+
+
+set DEBUG=*
+supervisor -i . index.js > app_out.log
+
